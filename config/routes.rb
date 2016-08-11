e95a0c0c576153e4a1c417147471da20ef7529f8
Mark Nuiver <mark@vrijblikken.nl>
2016-08-11 12:48:49 +0200

    Post likes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get 'user_posts/:user_id' => 'posts#user', as: :user_posts
 
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
